48850c012049373c26f666ff318517b1d17d78ae
clong desktop <chip_long@yahoo.com>
2023-01-02 11:10:49 -0700

    renamed Appointment file to AppointmentsDayView

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { sampleAppointments } from "../data/sampleData";
-import { AppointmentsDayView } from "./Appointment";
+import { AppointmentsDayView } from "./AppointmentsDayView";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
