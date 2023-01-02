983e463895bf18a7f4df435b4a9d2eefdedf7ffc
clong desktop <chip_long@yahoo.com>
2023-01-02 10:06:10 -0700

    Added in AppointmentsDayView

--- src/Appointment.js
+++ src/Appointment.js
@@ -1,1 +1,33 @@
-export const Appointment = ({ customer }) => customer.firstName;
+import React, { useState } from "react";
+
+const appointmentTimeOfDay = (time) => {
+  const [h, m] = new Date(time).toTimeString().split(":");
+  return `${h}:${m}`;
+};
+
+export const Appointment = ({ customer }) => <>{customer.firstName}</>;
+
+export const AppointmentsDayView = ({ appointments }) => {
+  const [selectedAppointment, setSelectedAppointment] = useState(0);
+
+  return (
+    <div id="appointmentsDayView">
+      {/* left column */}
+      <ol>
+        {appointments.map((appointment, i) => (
+          <li key={appointment.startsAt}>
+            <button type="button" onClick={() => setSelectedAppointment(i)}>
+              {appointmentTimeOfDay(appointment.startsAt)}
+            </button>
+          </li>
+        ))}
+      </ol>
+      {/* right column */}
+      {appointments.length === 0 ? (
+        <p>There are no appointments scheduled for today.</p>
+      ) : (
+        <Appointment {...appointments[selectedAppointment]} />
+      )}
+    </div>
+  );
+};
