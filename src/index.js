a8da7ac48a0a76c4e8a7d722541bdc5c47e32782
clong desktop <chip_long@yahoo.com>
2023-01-02 10:40:20 -0700

    added index file

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { sampleAppointments } from "../data/sampleData";
+import { AppointmentsDayView } from "./Appointment";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  <React.StrictMode>
+    <AppointmentsDayView appointments={sampleAppointments} />
+  </React.StrictMode>
+);
