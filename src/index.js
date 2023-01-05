7b67c8572b0fac9f92141844ab8daeee40636ad5
clong desktop <chip_long@yahoo.com>
2023-01-04 19:36:39 -0700

    added formatting to CusotmerForm (complete chap 4 pre exercises)

--- src/index.js
+++ src/index.js
@@ -2,11 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { sampleAppointments } from "../data/sampleData";
 import { AppointmentsDayView } from "./AppointmentsDayView";
+import { CustomerForm } from "./CustomerForm";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    <AppointmentsDayView appointments={sampleAppointments} />
+    <CustomerForm
+      original={{ firstName: "", lastName: "", phoneNumber: "" }}
+      onSubmit={console.log}
+    />
   </React.StrictMode>
 );
