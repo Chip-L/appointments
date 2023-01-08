26a4ee69c4760e8823ec9d331e3e2a0451dee43e
clong desktop <chip_long@yahoo.com>
2023-01-07 21:49:53 -0700

    added styling and hooked up to index.html

--- src/index.js
+++ src/index.js
@@ -1,31 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { sampleAppointments } from "../data/sampleData";
+import {
+  sampleAppointments,
+  sampleAvailableTimeSlots,
+} from "../data/sampleData";
 import { AppointmentForm } from "./AppointmentForm";
 import { AppointmentsDayView } from "./AppointmentsDayView";
 import { CustomerForm } from "./CustomerForm";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-const blankAppointment = {
-  service: "",
-};
-const today = new Date();
-const availableTimeSlots = [
-  { startsAt: today.setHours(9, 0, 0, 0) },
-  { startsAt: today.setHours(9, 30, 0, 0) },
-];
-
 root.render(
   <React.StrictMode>
+    <AppointmentsDayView appointments={sampleAppointments} />
     <AppointmentForm
-      original={blankAppointment}
-      availableTimeSlots={availableTimeSlots}
+      original={{}}
+      availableTimeSlots={sampleAvailableTimeSlots}
+      appointments={sampleAppointments}
+      onSubmit={console.log}
     />
-
-    {/* <CustomerForm
+    <CustomerForm
       original={{ firstName: "", lastName: "", phoneNumber: "" }}
       onSubmit={console.log}
-    /> */}
+    />
   </React.StrictMode>
 );
