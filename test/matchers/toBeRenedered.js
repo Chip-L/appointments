5d0314250995891ea519df0f49c8322cfbecf787
clong desktop <chip_long@yahoo.com>
2023-01-15 10:53:13 -0700

    toBeRendered returns pass is true when mock has been rendered

---
+++ test/matchers/toBeRenedered.js
@@ -0,0 +1,3 @@
+export const toBeRendered = () => {
+  return { pass: true };
+};
---
+++ test/matchers/toBeRenedered.test.js
@@ -0,0 +1,80 @@
+import React from "react";
+import { initializeReactContainer, render } from "../reactTestExtensions";
+import { stripTerminalColor } from "./matcherUtils";
+import { toBeRendered } from "./toBeRenedered";
+
+describe("toBeRendered", () => {
+  let Component;
+
+  beforeEach(() => {
+    initializeReactContainer();
+    Component = jest.fn(() => <div />);
+  });
+
+  it("returns pass is true when mock has been rendered", () => {
+    render(<Component />);
+    const result = toBeRendered(Component, {});
+    expect(result.pass).toBe(true);
+  });
+
+  // it("returns pass is false when mock has not been rendered", () => {
+  //   const result = toBeRendered(Component);
+  //   expect(result.pass).toBe(false);
+  // });
+
+  // it("returns pass is false when mock is null", () => {
+  //   const result = toBeRendered(null);
+  //   expect(result.pass).toBe(false);
+  // });
+
+  // it("returns pass is false when the properties do not match", () => {
+  //   render(<Component a="b" />);
+  //   const result = toBeRendered(Component, { c: "d" });
+  //   expect(result.pass).toBe(false);
+  // });
+
+  // it("returns pass is true when the properties of the first render match", () => {
+  //   render(<Component a="b" />);
+  //   render(<Component c="d" />);
+  //   const result = toBeRendered(Component, { a: "b" });
+  //   expect(result.pass).toBe(true);
+  // });
+
+  // it("returns a message that contains the source line if no match", () => {
+  //   render(<Component a="b" />);
+  //   const result = toBeRendered(Component, { c: "d" });
+  //   expect(stripTerminalColor(result.message())).toContain(
+  //     `expect(mockedComponent).toBeRendered({"c": "d"})`
+  //   );
+  // });
+
+  // it("returns a message that contains the source line if negated match", () => {
+  //   render(<Component c="d" />);
+  //   const result = toBeRendered(Component, { c: "d" });
+  //   expect(stripTerminalColor(result.message())).toContain(
+  //     `expect(mockedComponent).not.toBeRendered({"c": "d"})`
+  //   );
+  // });
+
+  // it("returns a message that the passed object is not a mock", () => {
+  //   const result = toBeRendered(<div />, {});
+  //   expect(stripTerminalColor(result.message())).toContain(
+  //     `mockedComponent is not a mock`
+  //   );
+  // });
+
+  // it("returns a message if mock has not been rendered", () => {
+  //   const result = toBeRendered(Component, {});
+  //   expect(stripTerminalColor(result.message())).toContain(
+  //     `Mocked component was never rendered`
+  //   );
+  // });
+
+  // it("returns a message if properties do not match", () => {
+  //   render(<Component a="b" />);
+  //   const result = toBeRendered(Component, { c: "d" });
+  //   expect(stripTerminalColor(result.message())).toContain(
+  //     `Rendered with props: {"a": "b"}`
+  //   );
+  // });
+});
