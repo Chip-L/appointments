06cc5d8c14db3ffee1b3ac12b176c191923b6a9a
clong desktop <chip_long@yahoo.com>
2023-01-15 14:40:09 -0700

    App displays the CustomerForm when button is clicked

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
 import { AppointmentsDayViewLoader } from "./AppointmentsDayViewLoader";
+import { CustomerForm } from "./CustomerForm";
 
 export const App = () => {
+  const [view, setView] = useState("dayView");
+
+  const transitionToAddCustomer = useCallback(() => setView("addCustomer"), []);
+
   return (
     <>
       <menu>
         <li>
-          <button type="button">Add customer and appointment</button>
+          <button type="button" onClick={transitionToAddCustomer}>
+            Add customer and appointment
+          </button>
         </li>
       </menu>
+      {view === "addCustomer" ? <CustomerForm /> : null}
       <AppointmentsDayViewLoader />
     </>
   );
--- test/App.test.js
+++ test/App.test.js
@@ -1,16 +1,22 @@
 import React from "react";
 import {
+  click,
   element,
   initializeReactContainer,
   render,
 } from "./reactTestExtensions";
 import { AppointmentsDayViewLoader } from "../src/AppointmentsDayViewLoader";
+import { CustomerForm } from "../src/CustomerForm";
 import { App } from "../src/App";
 
 jest.mock("../src/AppointmentsDayViewLoader", () => ({
   AppointmentsDayViewLoader: jest.fn(() => (
     <div id="AppointmentsDayViewLoader" />
   )),
+}));
+
+jest.mock("../src/CustomerForm", () => ({
+  CustomerForm: jest.fn(() => <div id="CustomerForm" />),
 }));
 
 describe("App", () => {
@@ -33,5 +39,11 @@ describe("App", () => {
     render(<App />);
     const firstButton = element("menu > li > button:first-of-type");
     expect(firstButton).toContainText("Add customer and appointment");
+  });
+
+  it("displays the CustomerForm when button is clicked", () => {
+    render(<App />);
+    click(element("menu > li > button:first-of-type"));
+    expect(element("#CustomerForm")).not.toBeNull();
   });
 });
